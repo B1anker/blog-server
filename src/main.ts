c250d3ed8348a636be953f0c101b83573fffe24b
b1anker <narutowontstop@gmail.com>
2019-06-29 01:44:08 +0800

    添加环境变量

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
       { from: /.*/, to: '/index.html' }
     ]
   }));
-  app.useStaticAssets(path.join(__dirname, '../../blog/dist'));
+  app.useStaticAssets(path.join(__dirname, process.env.NODE_ENV === 'development' ? '../../blog/dist' : '/root/blog/dist'));
   // 设置接口全局前缀
   app.setGlobalPrefix(API.prefix);
   // 添加gzip压缩
